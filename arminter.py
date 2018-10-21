330eb92725865479e5235a89eb80af89396a63c0
rohan1218 <44311973+rohan1218@users.noreply.github.com>
2018-10-21 06:23:01 -0500

    Create arminter.py

---
+++ arminter.py
@@ -0,0 +1,12 @@
+a=int(input())  
+b=int(input())
+  
+for num in range(a,b + 1):  
+   sum = 0  
+   temp = num  
+   while temp > 0:  
+       digit = temp % 10  
+       sum += digit ** 3  
+       temp //= 10  
+       if num == sum:  
+            print(num)
